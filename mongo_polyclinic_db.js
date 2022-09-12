d415eb533012ab982752b2f2553c875937b820cb
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-09-12 15:34:39 +0500

    add

---
+++ mongo_polyclinic_db.js
@@ -0,0 +1,172 @@
+use polyclinic;
+db.card_peerson.insertOne(
+{
+person_info: {
+    first_name: "first1",
+    last_name: "last1",
+    potronymic: "patronymic1",
+    age: "30",
+    contacts: {
+        address:
+        {
+        city: "city1",
+        street: "street1",
+        house: "house1",
+        flat: "flat"
+        },
+        telephones:
+        {
+        home: "+7999999999",
+        mobyle: "+7962999999"
+        }
+        },
+        med_card:
+        {
+        medical_history: "medical history1",
+        diagnoses:
+        {
+        diagnoses_1: "diagnoses1",
+        diagnoses_2: "diagnoses2",
+        diagnoses_3: "diagnoses3"
+        }
+        }
+
+}
+}
+)
+
+db.card_peerson.insertOne(
+{
+person_info: {
+    first_name: "first2",
+    last_name: "last2",
+    potronymic: "patronymic2",
+    age: "20",
+    contacts: {
+        address:
+        {
+        city: "city2",
+        street: "street2",
+        house: "house2",
+        flat: "flat"
+        },
+        telephones:
+        {
+        home: "+7922299999",
+        mobyle: "+79629922999"
+        }
+        },
+        med_card:
+        {
+        medical_history: "medical history2",
+        diagnoses:
+        {
+        diagnoses_1: "diagnoses12",
+        diagnoses_2: "diagnoses22",
+        diagnoses_3: "diagnoses32"
+        }
+        }
+
+}
+}
+)
+
+db.card_peerson.insertOne(
+{
+person_info: {
+    first_name: "first3",
+    last_name: "last3",
+    age: "33",
+    contacts: {
+        address:
+        {
+        city: "city3",
+        street: "street3",
+        house: "house3",
+        flat: "flat"
+        },
+        telephones:
+        {
+        home: "+7999339999"
+
+        }
+        },
+        med_card:
+        {
+        medical_history: "medical history3",
+        diagnoses:
+        {
+        diagnoses_1: "diagnoses1"
+
+        }
+        }
+
+}
+}
+)
+
+db.card_peerson.insertOne(
+{
+person_info: {
+    first_name: "first4",
+    last_name: "last14",
+    potronymic: "patronymic4",
+    age: "40",
+    contacts: {
+        address:
+        {
+        city: "city4",
+        street: "street4",
+        house: "house1"
+        },
+        telephones:
+        {
+        home: "+799888899",
+        mobyle: "+7962988889",
+        work: "+788559222"
+        }
+        },
+        med_card:
+        {
+        medical_history: "medical history1",
+        diagnoses:
+        {
+        diagnoses_1: "diagnoses1",
+        diagnoses_2: "diagnoses2",
+        diagnoses_3: "diagnoses3",
+        diagnoses_4: "diagnoses4",
+        }
+        }
+
+}
+}
+)
+
+db.card_peerson.insertOne(
+{
+person_info: {
+    first_name: "first5",
+    last_name: "last5",
+    potronymic: "patronymic5",
+    age: "38",
+    contacts: {
+        address:
+        {
+        city: "city5",
+        street: "street5",
+        house: "house5",
+        flat: "flat5"
+        },
+        telephones:
+        {
+        mobyle: "+7962999999"
+        }
+        },
+        med_card:
+        {
+        medical_history: "medical history1"
+        }
+
+}
+}
+)
